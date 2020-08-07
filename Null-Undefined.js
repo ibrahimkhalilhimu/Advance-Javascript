e36dd26b019649ed9e9463fefb1811600a920115
ibrahimkhalilhimu <ibrahimkhalilhemu1@gmail.com>
2020-08-07 09:57:35 +0600

    null-undefined

---
+++ Null-Undefined.js
@@ -0,0 +1,27 @@
+// let ar value na dile undefined
+let pakhi
+console.log(pakhi);
+
+// function ar return na korla undefined
+
+function add(num1,num2) {
+    console.log(num1 + num2);
+}
+const result = add(13,12)
+console.log(result);
+
+// return ar pora jodi kicu na day
+function add(num1,num2) {
+    console.log(num1 + num2);
+    return
+}
+const result = add(13,12)
+console.log(result);
+
+// function ar value na dile
+function add(num1,num2) {
+    const total =(num1 + num2);
+   return total
+}
+const result = add(13)
+console.log(result);
---
+++ true-false.js
@@ -0,0 +1,7 @@
+const age = 4
+if (age) {
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
